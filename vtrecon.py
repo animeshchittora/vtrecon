307bc97458b27ac40c8f8a42ee00f5262850d8bf
ANIMESH CHITTORA <animeshchittora_mukesh@srmuniv.edu.in>
2020-04-20 00:47:42 +0530

    Create vtrecon.py

---
+++ vtrecon.py
@@ -0,0 +1,51 @@
+import requests
+import json 
+import pyfiglet
+
+ascii_banner=pyfiglet.figlet_format("VT-RECON!")
+print(ascii_banner)
+
+print("\n")
+
+url = 'https://www.virustotal.com/vtapi/v2/domain/report'
+
+print("url exampple---------> site.com")
+data=input("Enter URl : ")
+print("\n")
+
+params = {'apikey':'<apikey>','domain':data}
+
+print("[1] for whois info")
+print("[2] for subdomains")
+pref=int(input("Enter Number : "))
+
+
+try:
+	response = requests.get(url, params=params)
+	jdata=response.json()
+	sub_domains=jdata['subdomains']
+	more_domains=jdata['last_https_certificate']['extensions']['subject_alternative_name']
+	
+
+
+	if(pref==1):
+		print("--------------------------------------whois info------------------------------")
+		print("\n")
+		whois=jdata['whois']
+		print(whois)
+		print("\n")
+
+	if(pref==2):
+		print("---------------------------------------subdomains----------------------------")
+		print("\n")
+		for sub in sub_domains:
+			print(sub)
+		for domain in more_domains:
+			print(domain)
+
+	if(pref is not 1 or pref is not 2):
+		print("Enter correct choice")
+		
+except(KeyError):
+	print("No sub-domains found")
+	exit(0)
